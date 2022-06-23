d839120e1bbc0dad9489956ca0ee41294a563863
Matt Philmon <Matt_Philmon@carmax.com>
2022-06-23 17:14:48 -0400

    Create an extension for use in dependency injection (Startup).

---
+++ CnpSdkForNet/CnpSdkForNet/Core/AddCnpSdk4Net.cs
@@ -0,0 +1,23 @@
+﻿using Cnp.Sdk;
+using Cnp.Sdk.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Cnp.Sdk.Core
+{
+    public static class CnpSdk4NetExtensions
+    {
+        public static IServiceCollection AddCnpSdk4Net(this IServiceCollection services,
+            Dictionary<string, string> config, X509Certificate2 cert =  null)
+        {
+            services.AddHttpClient<ICommunications, Communications>()
+                .ConfigurePrimaryHttpMessageHandler(sp =>
+                {
+                    return new CommunicationsHttpClientHandler(config, cert);
+                });
+
+            return services;
+        }
+    }
+}
